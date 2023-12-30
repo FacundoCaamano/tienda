ff77ba532f5dc0e944b797f3bf2cebcbd3800794
Facundo Caamaño <facundoadriancaamano@outlook.com>
2023-12-29 23:33:53 -0300

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './dashboard/components/footer/footer.component';
+import { NavbarComponent } from './dashboard/components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
